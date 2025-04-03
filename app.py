6594b4cf0d685f699545058f61f0a7a38fbd9f64
ViniElias <viniciuseliascr@gmail.com>
2025-04-03 09:59:57 -0300

    Add template agent

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ import requests
 import pytz
 import yaml
 from tools.final_answer import FinalAnswerTool
+import os
+from hf_token import HF_TOKEN
 
 from Gradio_UI import GradioUI
+
+os.environ["HF_TOKEN"] = HF_TOKEN
+
 
 # Below is an example of a tool that does nothing. Amaze us with your creativity !
 @tool
 def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
     #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
+    """A tool that does nothing yet
     Args:
         arg1: the first argument
         arg2: the second argument
